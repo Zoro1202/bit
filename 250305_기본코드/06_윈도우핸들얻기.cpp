4f4808d487c85370a55214d6a3576c09e96e95c2
bit <bit@DESKTOP-V3OB23V>
2025-03-06 08:59:40 +0900

    message

---
+++ 250305_기본코드/06_윈도우핸들얻기.cpp
@@ -0,0 +1,115 @@
+//06_윈도우핸들얻기.cpp
+/*
+[어떤사건이 발생했을 때] -> [어떤 일을 하고싶다]
+키보드 F 가 눌러졌을 때 -> 계산기를 화면에서 지우고 싶다.
+*/
+#pragma comment (linker, "/subsystem:windows")
+
+#include <Windows.h>
+#include <tchar.h>
+
+//메시지 처리 함수
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+	{
+		if (wParam == 'F')
+		{
+			SetWindowText(hwnd, TEXT("F 클릭"));
+			HWND h = FindWindow(0, TEXT("계산기"));
+			if (h == 0)
+			{
+				SetWindowText(hwnd, TEXT("못찾음"));
+			}
+			else
+			{
+				SetWindowText(hwnd, TEXT("찾음"));
+				ShowWindow(h, SW_HIDE);
+			}
+		}
+		else if(wParam == 'S')
+		{
+			HWND h = FindWindow(0, TEXT("계산기"));
+			if (h != 0)
+			{
+				ShowWindow(h, SW_SHOW);
+			}
+		}
+		return 0;
+	}
+
+	case WM_LBUTTONDOWN:
+	{
+		MessageBox(hwnd, TEXT("LBUTTON"), TEXT("확인"), MB_OK);
+		return 0;
+	}
+
+	//윈도우 생성되었고, 화면에 안보인다.
+	//초기화!!!!
+	case WM_CREATE:
+	{
+		return 0;
+	}
+
+	//DestroyWindow() 함수 내부에서 호출
+	//종료처리시점!!!!
+	case WM_DESTROY:
+	{
+		//App Q에 WM_QUIT 메시지 넣어준다.
+		PostQuitMessage(0);
+		return 0;
+	}
+	}
+
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+//시작 함수
+int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hprevInstance, LPTSTR cmdline, int showcmd)
+{
+	//1. 윈도우 클래스 정의
+	WNDCLASSEX wnd;
+	wnd.cbSize = sizeof(WNDCLASSEX);
+
+	wnd.style = CS_HREDRAW | CS_VREDRAW;  //  0;
+	wnd.hInstance = hInst;
+	wnd.lpfnWndProc = WndProc;  // DefWindowProc;	//미리 정의되어 있는 메시지 처리 프로시저
+	wnd.lpszClassName = TEXT("wb41");
+
+	//미리 만들어진 리소스를 사용하는 방법
+	wnd.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wnd.hCursor = LoadCursor(0, IDC_HAND);
+	//BRUSH, PEN, FONT
+	wnd.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wnd.lpszMenuName = 0;
+
+	wnd.cbClsExtra = 0;
+	wnd.cbWndExtra = 0;
+	wnd.hIconSm = 0;
+
+	//2. 윈도우 클래스 등록
+	RegisterClassEx(&wnd);
+
+	//3. 윈도우객체 (생성.. CreateXXX)
+	HWND hwnd = CreateWindowEx(WS_EX_TOPMOST, TEXT("wb41"), TEXT("첫번째 윈도우"),
+		WS_OVERLAPPEDWINDOW,// &~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX,
+		CW_USEDEFAULT, 0,
+		500, 500, //CW_USEDEFAULT, 0,
+		0, 0, hInst, 0);
+
+	//4. 윈도우 화면 출력
+	ShowWindow(hwnd, SW_SHOW); //showcmd : WinMain의 마지막인자	
+
+
+	//5. 메시지 루프
+	MSG msg;
+	//GetMessage : WM_QUIT를 가져왔을 때만 FALSE 반환
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
