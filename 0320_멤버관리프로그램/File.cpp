93d5ae4c8076a33aaa54cad53541a91fea0191ba
bit <bit@DESKTOP-V3OB23V>
2025-03-20 20:14:14 +0900

    0320 파일 입출력 수정

--- 0320_멤버관리프로그램/File.cpp
+++ 0320_멤버관리프로그램/File.cpp
@@ -2,6 +2,7 @@
 
 #define FILENAME_DEFAULT TEXT("mem.mem")
 
+//기본 파일 IO
 int file_load(vector<MEMBER*>& members)
 {
 	FILE* fp;
@@ -48,6 +49,7 @@ int file_save(vector<MEMBER*>& members)
 	return 1;
 }
 
+//파일 윗 파일네임
 int file_load_filename(vector<MEMBER*>& members, TCHAR* fname)
 {
 	FILE* fp;
@@ -94,24 +96,27 @@ int file_save_filename(vector<MEMBER*>& members, TCHAR* fname)
 	return 1;
 }
 
+//다이알로그 창 띄우기
 void file_dlg_open(HWND hdlg, vector<MEMBER*>& members)
 {
 	
 	OPENFILENAME ofn;              // 대화상자 정보를 담는 구조체
-	TCHAR szFile[260] = { 0 };      // 파일 경로를 저장할 버퍼
+	TCHAR szFile[260] = FILENAME_DEFAULT;      // 파일 경로를 저장할 버퍼
 
 	ZeroMemory(&ofn, sizeof(ofn));
-	ofn.lStructSize = sizeof(ofn);
-	ofn.hwndOwner = hdlg;          // 만약 창 핸들이 있다면 넣어줘도 돼
-	ofn.lpstrFile = szFile;
-	ofn.nMaxFile = sizeof(szFile);
-	ofn.lpstrFilter = TEXT("All\0*.*\0mem\0*.mem\0");  // 필터 설정
-	ofn.nFilterIndex = 1;
-	ofn.lpstrFileTitle = (LPTSTR)TEXT("mem.mem");
-	ofn.nMaxFileTitle = 0;
+	ofn.lpstrTitle = (LPTSTR)TEXT(".mem 파일 열기");// 창 캡션
+	ofn.lStructSize = sizeof(ofn); // 사이즈
+	ofn.hwndOwner = hdlg;          //부모 윈도우 핸들
+	ofn.lpstrFile = szFile;			// 파일 경로가 저장될 버퍼
+	ofn.nMaxFile = sizeof(szFile); // 위 버퍼의 사이즈
+	ofn.lpstrFilter = TEXT("All\0*.*\0mem\0*.mem\0"); // 필터
+	ofn.nFilterIndex = 2; // 초기 필터 인덱스
+	ofn.lpstrFileTitle = 0; // 경로 없는 버퍼
+	ofn.nMaxFileTitle = 0; // 버퍼 사이즈
 	ofn.lpstrInitialDir = NULL;    // 기본 디렉토리
-	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST; // OFN_PATHMUSTEXIST : 지정한 경로가 실제로 존재해야 한다는 걸 강제함.
+													  //  OFN_FILEMUSTEXIST : 선택한 파일 자체가 존재해야 한다.
+													 //	  잘못된 경로나 없는 파일 선택을 막기.
 	if (GetOpenFileName(&ofn) == TRUE) {
 		// 선택한 파일 경로는 ofn.lpstrFile 에 저장됨
 		MessageBox(hdlg, ofn.lpstrFile, TEXT("파일"), MB_OK);
@@ -122,17 +127,20 @@ void file_dlg_open(HWND hdlg, vector<MEMBER*>& members)
 void file_dlg_save(HWND hdlg, vector<MEMBER*>& members)
 {
 	OPENFILENAME ofn;
-	TCHAR szFile[260] = { 0 };
+	TCHAR szFile[260] = FILENAME_DEFAULT; // 파일의 경로 저장되는 버퍼
+	//기본 경로&이름 넣으면 첫 실행때 그 이름이 들어가있음
 
 	ZeroMemory(&ofn, sizeof(ofn));
-	ofn.lStructSize = sizeof(ofn);
+	ofn.lpstrTitle = (LPTSTR)TEXT(".mem 파일 저장"); // 창 캡션
+	ofn.lStructSize = sizeof(ofn); // 사이즈
+	ofn.lpstrDefExt = (LPTSTR)TEXT("mem"); // 확장명
 	ofn.hwndOwner = hdlg;  // 부모 윈도우 핸들을 지정하면 모달 창으로 뜸
-	ofn.lpstrFile = szFile;
-	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFile = szFile; // 파일 이름 경로 등 저장되는 버퍼
+	ofn.nMaxFile = sizeof(szFile); // 위 버퍼의 사이즈
 	ofn.lpstrFilter = TEXT("All\0*.*\0mem\0*.mem\0");  // 필터 설정
-	ofn.nFilterIndex = 1;
-	ofn.lpstrFileTitle = NULL;
-	ofn.nMaxFileTitle = 0;
+	ofn.nFilterIndex = 2; // 초기 필터 인덱스 1~
+	ofn.lpstrFileTitle = 0; // 경로 없이 저장하는 버퍼(NULL 로 해도 됨)
+	ofn.nMaxFileTitle = 0; // 위 버퍼의 사이즈
 	ofn.lpstrInitialDir = NULL;   // 기본 디렉토리
 	ofn.Flags = OFN_OVERWRITEPROMPT;  // 파일 덮어쓰기 경고
 
